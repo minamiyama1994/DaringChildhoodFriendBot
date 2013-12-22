3c6ff390bd28feb600af9ea4268561bcefef6498
南山まさかず <masakazu.minamiyama@gmail.com>
2013-12-23 05:18:49 +0900

    recreate solution

---
+++ DaringChildhoodFriendBot/DaringChildhoodFriendBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Console;
+using System.Xml.Serialization;
+using CoreTweet;
+using CoreTweet.Streaming;
+
+namespace DaringChildhoodFriendBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
